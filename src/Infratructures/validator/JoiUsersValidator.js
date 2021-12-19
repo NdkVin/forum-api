faabcb2dca5289371a54dc482b9620649bd806a3
Andika <andika.kavin@gmail.com>
2021-12-19 17:34:31 +0700

    JoiUsersValidator

---
+++ src/Infratructures/validator/JoiUsersValidator.js
@@ -0,0 +1,42 @@
+const UsersValidator = require('../../Applications/validator/UsersValidator');
+const InvariantError = require('../../Commons/exceptions/InvariantError');
+
+class JoiUsersValidator extends UsersValidator {
+  constructor(joi) {
+    super();
+    this._joi = joi;
+  }
+
+  validate(payload) {
+    const UsersSchema = this._joi.object({
+      username: this._joi.string().max(50).required().messages({
+        'string.base': 'username harus berupa string',
+        'string.empty': 'username tidak boleh kosong',
+        'string.max': 'username berisi maksimal 50 karakter',
+        'any.required': 'username harus diisi',
+      }),
+      password: this._joi.string().required().messages({
+        'string.base': 'password harus berupa string',
+        'string.empty': 'password tidak boleh kosong',
+        'any.required': 'password harus diisi',
+      }),
+      fullname: this._joi.string().required().messages({
+        'string.base': 'fullname harus berupa string',
+        'string.empty': 'fullname tidak boleh kosong',
+        'any.required': 'fullname harus diisi',
+      }),
+    });
+
+    if (!payload.username.match(/^[\w]+$/)) {
+      throw new InvariantError('username mengandung karakter terlarang');
+    }
+
+    const validateResult = UsersSchema.validate(payload);
+
+    if (validateResult.error) {
+      throw new InvariantError(validateResult.error.message);
+    }
+  }
+}
+
+module.exports = JoiUsersValidator;
---
+++ src/Infratructures/validator/_test/JoiUsersValidator.test.js
@@ -0,0 +1,63 @@
+const Joi = require('joi');
+const JoiUsersValidator = require('../JoiUsersValidator');
+
+describe('JoiUsersValidator', () => {
+  it('should throw Invariant error when send incomplete payload', () => {
+    const payload = {
+      username: 'andika',
+      password: 'andika',
+    };
+
+    const joiUsersValidator = new JoiUsersValidator(Joi);
+
+    expect(() => joiUsersValidator.validate(payload)).toThrowError('fullname harus diisi');
+  });
+
+  it('should return an error if sending a payload with the wrong data type', () => {
+    const payload = {
+      username: 'andika',
+      password: 'andika',
+      fullname: true,
+    };
+
+    const joiUsersValidator = new JoiUsersValidator(Joi);
+
+    expect(() => joiUsersValidator.validate(payload)).toThrowError('fullname harus berupa string');
+  });
+
+  it('should throw Invariant error when send username too long', () => {
+    const payload = {
+      username: 'andikaandikaandikaandikaandikaandikaandikaandikaandikaandikaandikaandika',
+      password: 'andika',
+      fullname: 'andika',
+    };
+
+    const joiUsersValidator = new JoiUsersValidator(Joi);
+
+    expect(() => joiUsersValidator.validate(payload)).toThrowError('username berisi maksimal 50 karakter');
+  });
+
+  it('should throw Invariant error when send username contain restrict character', () => {
+    const payload = {
+      username: 'andi ka',
+      password: 'andika',
+      fullname: 'andika',
+    };
+
+    const joiUsersValidator = new JoiUsersValidator(Joi);
+
+    expect(() => joiUsersValidator.validate(payload)).toThrowError('username mengandung karakter terlarang');
+  });
+
+  it('should not throw error', () => {
+    const payload = {
+      username: 'andika',
+      password: 'andika',
+      fullname: 'andika',
+    };
+
+    const joiUsersValidator = new JoiUsersValidator(Joi);
+
+    expect(() => joiUsersValidator.validate(payload)).not.toThrowError();
+  });
+});
