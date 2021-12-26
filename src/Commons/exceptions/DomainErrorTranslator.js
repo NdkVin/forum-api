1794a14aeb1a0d668a8da7208054c2c5a32292ed
ndkvin <andika.kavin@gmail.com>
2021-12-26 18:41:58 +0700

    domain error message

--- src/Commons/exceptions/DomainErrorTranslator.js
+++ src/Commons/exceptions/DomainErrorTranslator.js
@@ -9,6 +9,8 @@ const DomainErrorTranslator = {
 DomainErrorTranslator._directories = {
   'AUTH_USER.NOT_COMPLETE': new InvariantError('tidak dapat melakukan authentikasi karena properti yang dikirimkan tidak lengkap'),
   'AUTH_USER.NOT_MATCHING_DATA_TYPE': new InvariantError('tidak dapat melakukan authentikasi karena tipe data pada properti yang dikirimkan tidak sesuai'),
+  'ADDED_THREAD.NOT_CONTAIN_NEEDED_PROPERTY': new InvariantError('tidak dapat membuat thread baru karena data yang dikirim tidak lengkap'),
+  'ADDED_THREAD.NOT_MEET_DATA_TYPE_SPEC': new InvariantError('tidak dapat membuat thread baru karena data yang dikirim tidak sesaui'),
 };
 
 module.exports = DomainErrorTranslator;
--- src/Commons/exceptions/_test/DomainErrorTranslator.test.js
+++ src/Commons/exceptions/_test/DomainErrorTranslator.test.js
@@ -7,6 +7,10 @@ describe('DomainErrorTranslator', () => {
       .toStrictEqual(new InvariantError('tidak dapat melakukan authentikasi karena properti yang dikirimkan tidak lengkap'));
     expect(DomainErrorTranslator.translate(new InvariantError('AUTH_USER.NOT_MATCHING_DATA_TYPE')))
       .toStrictEqual(new InvariantError('tidak dapat melakukan authentikasi karena tipe data pada properti yang dikirimkan tidak sesuai'));
+    expect(DomainErrorTranslator.translate(new InvariantError('ADDED_THREAD.NOT_CONTAIN_NEEDED_PROPERTY')))
+      .toStrictEqual(new InvariantError('tidak dapat membuat thread baru karena data yang dikirim tidak lengkap'));
+    expect(DomainErrorTranslator.translate(new InvariantError('ADDED_THREAD.NOT_MEET_DATA_TYPE_SPEC')))
+      .toStrictEqual(new InvariantError('tidak dapat membuat thread baru karena data yang dikirim tidak sesaui'));
   });
 
   it('should return original error', () => {
