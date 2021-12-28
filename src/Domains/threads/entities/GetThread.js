f035daa205ce5db7adc74be7f5adeaf021c05bd1
ndkvin <andika.kavin@gmail.com>
2021-12-29 03:30:11 +0700

    get thread domain

---
+++ src/Domains/threads/entities/GetThread.js
@@ -0,0 +1,32 @@
+class GetThread {
+  constructor(payload) {
+    this._validate(payload);
+
+    const {
+      id, title, body, date, username,
+    } = payload;
+
+    this.id = id;
+    this.title = title;
+    this.body = body;
+    this.date = date;
+    this.username = username;
+    this.comments = [];
+  }
+
+  _validate(payload) {
+    const {
+      id, title, body, date, username,
+    } = payload;
+
+    if (!id || !title || !body || !date || !username) {
+      throw new Error('GET_THREAD.NOT_CONTAIN_NEEDED_PROPERTY');
+    }
+
+    if (typeof id !== 'string' || typeof title !== 'string' || typeof body !== 'string' || typeof date !== 'string' || typeof username !== 'string') {
+      throw new Error('GET_THREAD.NOT_MEET_DATA_TYPE_SPEC');
+    }
+  }
+}
+
+module.exports = GetThread;
---
+++ src/Domains/threads/entities/_test/GetThread.test.js
@@ -0,0 +1,44 @@
+const GetThread = require('../GetThread');
+
+describe('GetThread', () => {
+  it('should return error when send incomplete payload', () => {
+    const payload = {
+      id: 'thread-123',
+      title: 'ini title',
+    };
+
+    expect(() => new GetThread(payload))
+      .toThrowError('GET_THREAD.NOT_CONTAIN_NEEDED_PROPERTY');
+  });
+
+  it('should return error when send payload with incorrect data type', () => {
+    const payload = {
+      id: 'ini id',
+      title: true,
+      body: true,
+      date: 123,
+      username: 123,
+    };
+
+    expect(() => new GetThread(payload))
+      .toThrowError('GET_THREAD.NOT_MEET_DATA_TYPE_SPEC');
+  });
+
+  it('should retuning payload correctly', () => {
+    const payload = {
+      id: 'ini id',
+      title: 'true',
+      body: 'true',
+      date: '123',
+      username: '123',
+    };
+    const getThread = new GetThread(payload);
+
+    expect(getThread.id).toEqual(payload.id);
+    expect(getThread.title).toEqual(payload.title);
+    expect(getThread.body).toEqual(payload.body);
+    expect(getThread.date).toEqual(payload.date);
+    expect(getThread.username).toEqual(payload.username);
+    expect(getThread.comments).toEqual([]);
+  });
+});
