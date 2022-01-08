8d56fd9ae67dea091e7a09b3be7989adc7953cbb
ndkvin <andika.kavin@gmail.com>
2022-01-08 22:37:01 +0700

    like use case

---
+++ src/Applications/use_case/LikeCommentUseCase.js
@@ -0,0 +1,28 @@
+class LikeCommentUseCase {
+  constructor({
+    threadRepository,
+    commentrepository,
+    likeRepository,
+  }) {
+    this._threadRepository = threadRepository;
+    this._commentRepository = commentrepository;
+    this._likeRepository = likeRepository;
+  }
+
+  async execute(threadId, commentId, userId) {
+    await this._threadRepository.checkThreadById(threadId);
+    await this._commentRepository.checkCommentByIdAndThreadId(threadId, commentId);
+
+    const isLike = await this._likeRepository.checkLike(threadId, commentId, userId);
+
+    if (!isLike) {
+      const res = await this._likeRepository.like(threadId, commentId, userId);
+      return res;
+    }
+
+    const res = await this._likeRepository.unlike(threadId, commentId, userId);
+    return res;
+  }
+}
+
+module.exports = LikeCommentUseCase;
---
+++ src/Applications/use_case/_test/LikeCommentUseCase.test.js
@@ -0,0 +1,64 @@
+const ThreadRepository = require('../../../Domains/threads/ThreadRepository');
+const CommentRepository = require('../../../Domains/comments/CommentRepository');
+const LikeRepository = require('../../../Domains/likes/LikeRepository');
+const LikeCommentUseCase = require('../LikeCommentUseCase');
+
+describe('LikeCommentUseCase', () => {
+  it('should orchesting the like comment action correctly', async () => {
+    const threadId = 'thread-123';
+    const commentId = 'comment-123';
+    const userId = 'user-123';
+
+    const mockThreadRepository = new ThreadRepository();
+    const mockCommentrepository = new CommentRepository();
+    const mockLikeRepository = new LikeRepository();
+
+    mockThreadRepository.checkThreadById = jest.fn(() => Promise.resolve);
+    mockCommentrepository.checkCommentByIdAndThreadId = jest.fn(() => Promise.resolve);
+    mockLikeRepository.checkLike = jest.fn(() => Promise.resolve(0));
+    mockLikeRepository.like = jest.fn(() => Promise.resolve('like'));
+
+    const likeCommentUseCase = new LikeCommentUseCase({
+      threadRepository: mockThreadRepository,
+      commentrepository: mockCommentrepository,
+      likeRepository: mockLikeRepository,
+    });
+
+    const result = await likeCommentUseCase.execute(threadId, commentId, userId);
+
+    expect(mockThreadRepository.checkThreadById).toBeCalledWith(threadId);
+    expect(mockCommentrepository.checkCommentByIdAndThreadId).toBeCalledWith(threadId, commentId);
+    expect(mockLikeRepository.checkLike).toBeCalledWith(threadId, commentId, userId);
+    expect(mockLikeRepository.like).toBeCalledWith(threadId, commentId, userId);
+    expect(result).toEqual('like');
+  });
+
+  it('should orchesting the unlike comment action correctly', async () => {
+    const threadId = 'thread-123';
+    const commentId = 'comment-123';
+    const userId = 'user-123';
+
+    const mockThreadRepository = new ThreadRepository();
+    const mockCommentrepository = new CommentRepository();
+    const mockLikeRepository = new LikeRepository();
+
+    mockThreadRepository.checkThreadById = jest.fn(() => Promise.resolve);
+    mockCommentrepository.checkCommentByIdAndThreadId = jest.fn(() => Promise.resolve);
+    mockLikeRepository.checkLike = jest.fn(() => Promise.resolve(1));
+    mockLikeRepository.unlike = jest.fn(() => Promise.resolve('unlike'));
+
+    const likeCommentUseCase = new LikeCommentUseCase({
+      threadRepository: mockThreadRepository,
+      commentrepository: mockCommentrepository,
+      likeRepository: mockLikeRepository,
+    });
+
+    const result = await likeCommentUseCase.execute(threadId, commentId, userId);
+
+    expect(mockThreadRepository.checkThreadById).toBeCalledWith(threadId);
+    expect(mockCommentrepository.checkCommentByIdAndThreadId).toBeCalledWith(threadId, commentId);
+    expect(mockLikeRepository.checkLike).toBeCalledWith(threadId, commentId, userId);
+    expect(mockLikeRepository.unlike).toBeCalledWith(threadId, commentId, userId);
+    expect(result).toEqual('unlike');
+  });
+});
